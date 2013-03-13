8645f2b47b6ad4f9a1f580646995e3dfd30bbd26
Alexander <tigre4666@gmailc.com>
2013-03-13 20:39:43 +0300

    Updated

---
+++ protected/helpers/DFileHelper.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* @author ElisDN <mail@elisdn.ru>
+ * @link http://www.elisdn.ru
+ */
+class DFileHelper
+{
+    public static function getRandomFileName($path, $extension='')
+    {
+        $extension = $extension ? '.' . $extension : '';
+        $path = $path ? $path . '/' : '';
+ 
+        do {
+            $name = md5(microtime());
+            $file = $path . $name . $extension;
+            usleep(1);
+        } while (file_exists($file));
+ 
+        return $name;
+    }
+}
+
+?>
